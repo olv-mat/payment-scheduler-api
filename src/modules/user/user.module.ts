232c57ad13a58759b0b0f6a4ef76e6a6297dc67e
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-19 06:50:31 -0300

    chore: generate user module

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './presentation/user.controller';
+
+@Module({
+  controllers: [UserController],
+  providers: [],
+})
+export class UserModule {}
