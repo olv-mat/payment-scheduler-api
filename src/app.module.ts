92fbde8edeb5d558453c12d4719746cc8699f7fc
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-20 06:41:32 -0300

    chore: add typeorm support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow<string>('DATABASE_HOST'),
+        port: configService.getOrThrow<number>('DATABASE_PORT'),
+        username: configService.getOrThrow<string>('DATABASE_USERNAME'),
+        password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
+        database: configService.getOrThrow<string>('DATABASE_NAME'),
+        entities: [],
+        autoLoadEntities: false,
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/user/domain/repositories/user.repository.ts
+++ src/modules/user/domain/repositories/user.repository.ts
@@ -1,9 +1,13 @@
 import { UserEntity } from '../entities/user.entity';
 import { UpdateUserPayload } from '../types/update-user-payload.type';
 
-export abstract class UserAbstractRepository {
+export abstract class UserRepository {
   public abstract findAll(): Promise<UserEntity[]>;
-  public abstract findOne(id: string): Promise<UserEntity | null>;
-  public abstract update(id: string, payload: UpdateUserPayload): Promise<void>;
+  public abstract findById(id: string): Promise<UserEntity | null>;
+  public abstract findByEmail(email: string): Promise<UserEntity | null>;
+  public abstract update(
+    id: string,
+    payload: UpdateUserPayload,
+  ): Promise<UserEntity>;
   public abstract delete(id: string): Promise<void>;
 }
