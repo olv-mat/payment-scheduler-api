30efc6993ea5d7ed694277c5a7ca9dea021bb8b8
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-23 06:35:40 -0300

    fix: adjust user repositories

--- src/modules/user/domain/repositories/user.repository.ts
+++ src/modules/user/domain/repositories/user.repository.ts
@@ -5,9 +5,6 @@ export abstract class UserRepository {
   public abstract findAll(): Promise<UserEntity[]>;
   public abstract findById(id: string): Promise<UserEntity | null>;
   public abstract findByEmail(email: string): Promise<UserEntity | null>;
-  public abstract update(
-    id: string,
-    payload: UpdateUserPayload,
-  ): Promise<UserEntity>;
+  public abstract update(id: string, payload: UpdateUserPayload): Promise<void>;
   public abstract delete(id: string): Promise<void>;
 }
