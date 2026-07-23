6dd60526de02558a9a8d9ac52817d2b5d8fdc4d2
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-23 06:08:13 -0300

    chore: add typeorm support

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,15 +1,11 @@
 import { Roles } from '../enums/roles.enum';
 
 export class UserEntity {
-  public readonly name: string;
-  public readonly email: string;
-  public readonly password: string;
-  public readonly role: Roles;
-
-  constructor(name: string, email: string, password: string, role: Roles) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.role = role;
-  }
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly role: Roles,
+  ) {}
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserTypeOrmEntity } from './infrastructure/persistence/user.typeorm.entity';
 import { UserController } from './presentation/user.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserTypeOrmEntity])],
   controllers: [UserController],
   providers: [],
 })
