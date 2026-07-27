5a1f499b609c17520621033857514096a290f138
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-27 07:07:29 -0300

    docs: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Payment Scheduler API')
+      .setDescription(
+        'A NestJS-based API that allows users to schedule future payments, leveraging BullMQ and Redis for background processing, secured with JWT authentication.',
+      )
+      .addBearerAuth()
+      .build(),
+  );
+  const theme = new SwaggerTheme();
+  SwaggerModule.setup('/api', app, document, {
+    swaggerOptions: {
+      customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
