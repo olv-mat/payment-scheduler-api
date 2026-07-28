810f5228aa17669d96fd667a807ee8b3dd0a26b9
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-07-28 07:08:04 -0300

    docs: document find all users endpoint

--- src/modules/user/presentation/user.controller.ts
+++ src/modules/user/presentation/user.controller.ts
@@ -1,4 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  SwaggerInternalServerError,
+  SwaggerOperation,
+} from 'src/shared/presentation/swagger/swagger.decorators';
 import { FindAllUsersUseCase } from '../application/use-cases/find-all-users.usecase';
 import { UserResponseDto } from './dtos/user-response.dto';
 
@@ -7,6 +11,8 @@ export class UserController {
   constructor(private readonly findAllUseCase: FindAllUsersUseCase) {}
 
   @Get()
+  @SwaggerOperation('Retrieve all users')
+  @SwaggerInternalServerError()
   public async findAll(): Promise<UserResponseDto[]> {
     const entities = await this.findAllUseCase.execute();
     return UserResponseDto.fromEntities(entities);
