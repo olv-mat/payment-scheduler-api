2ffb502b6abc62bc3fc254bd67f78fb7a9f159fb
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-08-18 17:38:11 -0300

    feat: add login endpoint

--- src/modules/authentication/application/use-cases/register.usecase.ts
+++ src/modules/authentication/application/use-cases/register.usecase.ts
@@ -2,24 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserUseCase } from 'src/modules/user/application/use-cases/create-user.usecase';
 import { UserEntity } from 'src/modules/user/domain/entities/user.entity';
 import { CreateUserPayload } from 'src/modules/user/domain/types/create-user-payload.type';
-import { Signer } from 'src/shared/domain/cryptography/signer';
 
 @Injectable()
 export class RegisterUseCase {
-  constructor(
-    private readonly createUserUseCase: CreateUserUseCase,
-    private readonly signer: Signer,
-  ) {}
+  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
-  public async execute(
-    payload: CreateUserPayload,
-  ): Promise<[UserEntity, string]> {
-    const entity = await this.createUserUseCase.execute(payload);
-    const token = await this.signer.sign({
-      sub: entity.id,
-      name: entity.name,
-      email: entity.email,
-    });
-    return [entity, token];
+  public async execute(payload: CreateUserPayload): Promise<UserEntity> {
+    return await this.createUserUseCase.execute(payload);
   }
 }
--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
+import { AuthenticationFacade } from './application/authentication.facade';
+import { LoginUseCase } from './application/use-cases/login.usecase';
 import { RegisterUseCase } from './application/use-cases/register.usecase';
 import { AuthenticationController } from './presentation/authentication.controller';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthenticationController],
-  providers: [RegisterUseCase],
+  providers: [AuthenticationFacade, RegisterUseCase, LoginUseCase],
 })
 export class AuthenticationModule {}
