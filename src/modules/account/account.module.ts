2cb2d81dc5132f85cfe7be6cababbef6716ee94a
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-09-04 07:29:31 -0300

    feat: add create account use case

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateAccountUseCase } from './application/use-cases/create-account.usecase';
+import { AccountRepository } from './domain/repositories/account.repository';
 import { AccountTypeOrmEntity } from './infrastructure/persistence/account.typeorm.entity';
+import { AccountTypeOrmRepository } from './infrastructure/repositories/account.typeorm.repository';
 import { AccountController } from './presentation/account.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountTypeOrmEntity])],
   controllers: [AccountController],
-  providers: [],
+  providers: [
+    CreateAccountUseCase,
+    {
+      provide: AccountRepository,
+      useClass: AccountTypeOrmRepository,
+    },
+  ],
 })
 export class AccountModule {}
