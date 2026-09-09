f0fde26ccc477aa01fe6be310aa20bd1b9a3170d
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-09-09 07:17:40 -0300

    refactor: remove account controller

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -4,11 +4,10 @@ import { CreateAccountUseCase } from './application/use-cases/create-account.use
 import { AccountRepository } from './domain/repositories/account.repository';
 import { AccountTypeOrmEntity } from './infrastructure/persistence/account.typeorm.entity';
 import { AccountTypeOrmRepository } from './infrastructure/repositories/account.typeorm.repository';
-import { AccountController } from './presentation/account.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountTypeOrmEntity])],
-  controllers: [AccountController],
+  controllers: [],
   providers: [
     CreateAccountUseCase,
     {
